65f453eb34e740a8c34d30a958b96936bf8d784c
NewKanvas <137099346+NewKanvas@users.noreply.github.com>
2023-09-06 16:18:01 -0300

    Delete Python directory

--- Python/Codes/input.py
+++
@@ -1,8 +0,0 @@
-x = input("Digite uma mensagem:")
-print(x)
-y = input("Digite outra mensagem:")
-print(x+y)
-
-z = int(input("Digite um numero:"))
-s = int(input("Digite outro numero:"))
-print(z+s)
