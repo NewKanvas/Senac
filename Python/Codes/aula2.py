f308611181030d580cb25dc96adedf3bcaadbfd4
NewKanvas <137099346+NewKanvas@users.noreply.github.com>
2023-09-06 16:18:31 -0300

    Atualização

---
+++ Python/Codes/aula2.py
@@ -0,0 +1,10 @@
+#Valores
+salarioAtual = 2000
+r = 20
+
+#Calculo
+calc = (r/100)*salarioAtual
+novoSalario = salarioAtual+calc
+
+#Resultado
+print("\nO salário era", salarioAtual,"\nO aumento foi de",r,"%,\no novo valor do salario é", novoSalario,"\n")
