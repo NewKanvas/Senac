506bb59c630f52074dcee00d9475d223ee348cdb
NewKanvas <cassioramaca@gmail.com>
2023-09-18 13:41:58 -0300

    Atualizaçao

---
+++ Python/CodigosdaAula/input.py
@@ -0,0 +1,8 @@
+x = input("Digite uma mensagem:")
+print(x)
+y = input("Digite outra mensagem:")
+print(x+y)
+
+z = int(input("Digite um numero:"))
+s = int(input("Digite outro numero:"))
+print(z+s)
---
+++ Python/ExerciciosdoLivro/Multiplo.py
@@ -0,0 +1,6 @@
+x = int(input("Digite um numero:"))
+
+if x%5 == 0:
+    print("\n",x,"é multiplo de cinco\n")
+else:
+    print("\n",x,"não é multiplo de cinco\n")
