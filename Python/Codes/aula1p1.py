8c006c3d5ebe544c5e5031915c933806ee20af9c
NewKanvas <cassioramaca@gmail.com>
2023-09-18 13:36:23 -0300

    a

---
+++ Python/Codes/aula1p1.py
@@ -0,0 +1,10 @@
+x= 2
+y= 3
+
+
+mult = x*y
+div = x//y
+som = x+y
+subt = x-y
+
+print (mult,div,som,subt)
--- Python/Codes/inf.py
+++
@@ -1,7 +0,0 @@
-x = int(input('Digite N:'))
-
-
-for x in range (0,x,1):
-    if (x % 3 == 0):
-        print (x) 
-    
