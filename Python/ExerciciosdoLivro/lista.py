683d444a6460040a29fba7d262c54483e3dc56c0
NewKanvas <cassioramaca@gmail.com>
2023-09-22 16:21:54 -0300

    a

--- Python/ExerciciosdoLivro/lista.py
+++ Python/ExerciciosdoLivro/lista.py
@@ -1,5 +1,45 @@
-x = ("C", "b")
-y = ["C", "b"]
+x  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11,12,13,14,15]
+y  = [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
-print(type(x))
-print(type(y))
+def algo(x,y):
+    xy =[]
+    xpy = []
+    
+
+
+    i = 0
+    for z in range(0,15,1):
+        xy.append(x[i])
+        xy.append(y[i])
+        i = i+1
+
+    print("\nLista de elementos no mesmo indice:")
+    print(xy)
+
+    i = 0
+    for z in range(0,15,1):
+        print(x[i],"+",y[i],"=",x[i]+y[i])
+
+        xpy.append(x[i]+y[i])
+
+        i = i+1
+
+    print("\nLista de soma dos elementos de cada lista:")
+    print(xpy)
+
+    xny = []
+
+    i= 0
+    for i in range(len(x)):
+        if x[i] % 2 == 0:
+            xny.append(x[i])
+
+    for i in range(len(y)):
+        if y[i] % 2 == 0:
+            xny.append(y[i])
+        
+
+    print("\nListas sem numeros impares:")
+    print(xny)
+
+algo(x,y)
