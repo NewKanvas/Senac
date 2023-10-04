0dfeeb340fb765cf4dd0ad15b126b81d356aa60a
NewKanvas <cassioramaca@gmail.com>
2023-10-04 17:00:37 -0300

    Update aula2listas.py

--- Python/Exercicios/Modulo2/aula2listas.py
+++ Python/Exercicios/Modulo2/aula2listas.py
@@ -1,4 +1,4 @@
-x = [1, 2]
+"""x = [1, 2]
 y = [3, 4]
 
 z = x + y
@@ -15,3 +15,13 @@ y = 2
 
 z = x + y
 print(z)
+"""
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+lista3 = []
+
+for x in range(0, len(lista1)):
+    lista3.append(lista1[x] * lista2[x])
+
+print(lista3)
