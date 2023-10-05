d7d54a5b28d190ba603578a95698ad0bcd1efedc
NewKanvas <cassioramaca@gmail.com>
2023-10-05 14:14:16 -0300

    Update index.py

--- Python/Projetos-Senac/Projeto 2 Ind/index.py
+++ Python/Projetos-Senac/Projeto 2 Ind/index.py
@@ -2,36 +2,40 @@ x = "eX_tX_pX_sX"
 op = ("e", "t", "p", "s")
 listC = ["e5_t10_p8_s8", "e10_t7_p7_s8", "e8_t5_p4_s9", "e2_t2_p2_s1", "e10_t10_p8_s9"]
 listY = []
+listX = {}
 
-print("**Menu inicial**")
+print("**Filtragem**")
 
+for j in range(len(listC)):
+    nLista = listC[j].split("_")
+    #print(nLista)
 
-for j in range(len(op)):
-    underline = listC[1].find("_")
-    e = listC[1][:underline]
-    print(f"Avaliação de '{op[j]}': {e}")
+    listYD = {}
+
+    for i in range(len(nLista)):
+        valor = int(nLista[i][1:])
+        listYD[op[i]] = valor
+    listY.append(listYD)
+    
+'''print(listY)
+print(len(listY))'''
 
 
 def pedirNota():
     for i in range(len(op)):
-        y = input(f'Digite o valor minimo da nota "{op[i]}"')
-        listY.append(y)
+        y = int(input(f'Digite o valor minimo da nota "{op[i]}"'))
+        listX[op[i]] = y
+    print(listX)
+        
+    for i in range(len(listY)):
+        for j in range(len(op)):
+            '''elemento = listY[i][op[j]]
+            print(f"Elemento relacionado à letra '{op[j]}' no dicionário {i} da lista: {elemento}")'''
 
+            if listY[i][op[j]] < listX[op[j]]:
+                print(f"Candidato {i + 1} é menor em '{op[j]}'")
+            elif listY[i][op[j]] > listX[op[j]]:
+                print(f"Candidato {i + 1} é maior em '{op[j]}'")
 
-# pedirNota()
+pedirNota()
 
-"""
-def veri():
-    for j in range(len(op)):  # Dentro de um elemento só verifica o numero de letras
-
-        letra = listC[0].find(op[j])
-
-        underline = listC[0].find("_", j)
-        # Como fazer para ele atualizar a posição do underline
-
-        print(listC[0])
-        print(f"{op[j]} = {listC[0][letra + 1 : underline]}")
-
-
-veri()
-"""
