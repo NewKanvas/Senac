993bf3f81ec235acc7c1f742eafe6ce562d6eb8d
NewKanvas <cassioramaca@gmail.com>
2023-10-11 14:59:25 -0300

    up

---
+++ Python/Exercicios/Modulo2/dicionarios.py
@@ -0,0 +1,18 @@
+inglesPort = {"one": "um", "two": "dois", "three": "três"}
+
+print(inglesPort.keys())
+
+inglesPort = {"one": "um", "two": "dois", "three": "três"}
+
+print(inglesPort.values())
+
+inglesPort = {"one": "um", "two": "dois", "three": "três"}
+
+print(inglesPort.values())
+print(inglesPort.keys())
+
+inglesPort = {"one": "um", "two": "dois", "three": "três"}
+
+print(inglesPort.values())
+print(inglesPort.keys())
+print(inglesPort.items())
--- Python/Tutoriais/dcio.py
+++ Python/Tutoriais/dcio.py
@@ -3,10 +3,3 @@ nome2 = input("Digite o nome de quem mora no Rio de Janeiro:\n")
 teste = {"Rio de Janeiro": {nome1, nome2}}
 
 print(teste)
-
-
-"""
-
-Candidato 1: Valor 1, Valor 2
-
-"""
