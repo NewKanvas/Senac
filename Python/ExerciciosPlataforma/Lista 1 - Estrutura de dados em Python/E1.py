aa7a16be8601856419524559ae631c032d064fb7
NewKanvas <cassioramaca@gmail.com>
2023-10-11 13:33:05 -0300

    up

---
+++ Python/ExerciciosPlataforma/Lista 1 - Estrutura de dados em Python/E1.py
@@ -0,0 +1,14 @@
+"""
+O que é para fazer:
+
+Crie uma fila de espera com os nomes de cinco pessoas e execute as seguintes operações: 1-Imprima na tela a fila atual. 2-Remova o primeiro elemento da fila e imprima na tela a fila atualizada. 3-Adicione mais dois nomes à fila e imprima na tela a fila atualizada.
+
+Como Fazer:
+
+    Crie uma lista com cinco nomes de pessoas, que representará a fila de espera;
+    Utilize a função print() para imprimir na tela a fila atual;
+    Utilize o método pop(0) para remover o primeiro elemento da lista;
+    Utilize novamente a função print() para imprimir a fila atualizada;
+    Utilize o método append() para adicionar dois novos nomes à lista;
+    Novamente, utilize a função print() para imprimir a fila atualizada.
+"""
